1b6e02537c7c690dbc1f1e176dcd1a0012c6d019
sukorenomw <sukorenomw@gmail.com>
2016-04-27 23:42:13 +0700

    added read and play sound

---
+++ Program/controller/audioPlayer.py
@@ -0,0 +1,73 @@
+import sys
+
+from PyQt4 import QtCore, QtGui
+
+try:
+    from PyQt4.phonon import Phonon
+except ImportError:
+    app = QtGui.QApplication(sys.argv)
+    QtGui.QMessageBox.critical(None, "Music Player",
+            "Your Qt installation does not have Phonon support.",
+            QtGui.QMessageBox.Ok | QtGui.QMessageBox.Default,
+            QtGui.QMessageBox.NoButton)
+    sys.exit(1)
+
+class AudioPlayer():
+    def __init__(self, volumeSlider, sliderSeeker, lcdNumber, audioPlayBtn, audioPauseBtn, audioStopBtn):
+        self.volumeSlider = volumeSlider
+        self.seekSlider = sliderSeeker
+        self.lcdNumber = lcdNumber
+        self.audioPlayBtn = audioPlayBtn
+        self.audioPauseBtn = audioPauseBtn
+        self.audioStopBtn = audioStopBtn
+        self.initAudioPlayer()
+
+    def setAudioSource(self, audiofile):
+        self.mediaObject.setCurrentSource(Phonon.MediaSource(audiofile))
+
+    def initAudioPlayer(self):
+        self.audioOutput = Phonon.AudioOutput(Phonon.MusicCategory)
+        self.mediaObject = Phonon.MediaObject()
+        self.metaInformationResolver = Phonon.MediaObject()
+
+        self.mediaObject.setTickInterval(1000)
+        self.mediaObject.tick.connect(self.tick)
+        self.mediaObject.stateChanged.connect(self.stateChanged)
+        self.mediaObject.finished.connect(self.mediaObject.stop)
+
+        Phonon.createPath(self.mediaObject, self.audioOutput)
+
+        self.volumeSlider.setAudioOutput(self.audioOutput)
+        self.volumeSlider.setSizePolicy(QtGui.QSizePolicy.Maximum,
+                                        QtGui.QSizePolicy.Maximum)
+
+        self.seekSlider.setMediaObject(self.mediaObject)
+
+    def tick(self, time):
+        displayTime = QtCore.QTime(0, (time / 60000) % 60, (time / 1000) % 60)
+        self.lcdNumber.display(displayTime.toString('mm:ss'))
+
+    def stateChanged(self, newState, oldState):
+        if newState == Phonon.ErrorState:
+            if self.mediaObject.errorType() == Phonon.FatalError:
+                QtGui.QMessageBox.warning(self, "Fatal Error",
+                                          self.mediaObject.errorString())
+            else:
+                QtGui.QMessageBox.warning(self, "Error",
+                                          self.mediaObject.errorString())
+
+        elif newState == Phonon.PlayingState:
+            self.audioPlayBtn.setEnabled(False)
+            self.audioPauseBtn.setEnabled(True)
+            self.audioStopBtn.setEnabled(True)
+
+        elif newState == Phonon.StoppedState:
+            self.audioStopBtn.setEnabled(False)
+            self.audioPlayBtn.setEnabled(True)
+            self.audioPauseBtn.setEnabled(False)
+            self.lcdNumber.display("00:00")
+
+        elif newState == Phonon.PausedState:
+            self.audioPauseBtn.setEnabled(False)
+            self.audioStopBtn.setEnabled(True)
+            self.audioPlayBtn.setEnabled(True)
--- Program/controller/trainingWindowController.py
+++ Program/controller/trainingWindowController.py
@@ -1,60 +1,44 @@
-import sys
+# coding=utf-8
 import view.trainingWindow as trainingWindow
-
-
+import audioPlayer
 from PyQt4 import QtCore, QtGui
-try:
-    from PyQt4.phonon import Phonon
-except ImportError:
-    app = QtGui.QApplication(sys.argv)
-    QtGui.QMessageBox.critical(None, "Music Player",
-            "Your Qt installation does not have Phonon support.",
-            QtGui.QMessageBox.Ok | QtGui.QMessageBox.Default,
-            QtGui.QMessageBox.NoButton)
-    sys.exit(1)
 
 
 class MainWindow(QtGui.QMainWindow, trainingWindow.Ui_MainWdw):
-
     def __init__(self):
         super(self.__class__, self).__init__()
         self.setupUi(self)
-        self.initAudioPlayer()
+        self.player = audioPlayer.AudioPlayer(self.volumeSlider,
+                                              self.seekSlider,
+                                              self.lcdNumber,
+                                              self.audioPlayBtn,
+                                              self.audioPauseBtn,
+                                              self.audioStopBtn)
         self.initUI()
         self.actionExit.triggered.connect(self.close)
+        self.actionAbout_Qt.triggered.connect(QtGui.qApp.aboutQt)
+        self.actionAbout.triggered.connect(self.about)
         self.openAudioBtn.clicked.connect(self.showOpenDialog)
 
+    def about(self):
+        QtGui.QMessageBox.information(self, "Text Dependent Speaker Verification",
+                                      "Text Dependent Speaker Verification - the "
+                                      "Final project software to identify and verify Speaker based on their speech.\n\n"
+                                      "\xa9 Sukoreno Mukti - 1112051 \n Informatics Engineering Dept. ITHB")
+
     def showOpenDialog(self):
-        self.audioFile = QtGui.QFileDialog.getOpenFileName(self,'Open audio file',
-                         '',
-                         "Audio Files (*.mp3 *.wav *.ogg)",
-                         None, QtGui.QFileDialog.DontUseNativeDialog)
+        self.audioFile = QtGui.QFileDialog.getOpenFileName(self, 'Open audio file',
+                                                           '',
+                                                           "Audio Files (*.mp3 *.wav *.ogg)",
+                                                           None, QtGui.QFileDialog.DontUseNativeDialog)
         fileName = str(self.audioFile)
-        self.audioFilenameLbl.setText(fileName[fileName.rfind('/')+1:len(fileName)])
+        self.audioFilenameLbl.setText(fileName[fileName.rfind('/') + 1:len(fileName)])
         self.audioPlayBtn.setDisabled(False)
-        self.audioPlayBtn.clicked.connect(self.mediaObject.play)
-        self.audioPauseBtn.clicked.connect(self.mediaObject.pause)
-        self.audioStopBtn.clicked.connect(self.mediaObject.stop)
-
-        self.mediaObject.setCurrentSource(Phonon.MediaSource(self.audioFile))
-
-    def initAudioPlayer(self):
-        self.audioOutput = Phonon.AudioOutput(Phonon.MusicCategory, self)
-        self.mediaObject = Phonon.MediaObject(self)
-        self.metaInformationResolver = Phonon.MediaObject(self)
-
-        self.mediaObject.setTickInterval(1000)
-        self.mediaObject.tick.connect(self.tick)
-        self.mediaObject.stateChanged.connect(self.stateChanged)
-        self.mediaObject.finished.connect(self.mediaObject.stop)
-
-        Phonon.createPath(self.mediaObject, self.audioOutput)
-
-        self.volumeSlider.setAudioOutput(self.audioOutput)
-        self.volumeSlider.setSizePolicy(QtGui.QSizePolicy.Maximum,
-                                        QtGui.QSizePolicy.Maximum)
-
-        self.seekSlider.setMediaObject(self.mediaObject)
+        self.audioPlayBtn.clicked.connect(self.player.mediaObject.play)
+        self.audioPauseBtn.clicked.connect(self.player.mediaObject.pause)
+        self.audioStopBtn.clicked.connect(self.player.mediaObject.stop)
+        self.extractSaveBtn.setDisabled(False)
+        self.player.setAudioSource(self.audioFile)
 
     def initUI(self):
         palette = QtGui.QPalette()
@@ -68,35 +52,6 @@ class MainWindow(QtGui.QMainWindow, trainingWindow.Ui_MainWdw):
         self.trainProgress.setValue(0)
         self.lcdNumber.display("00:00")
         self.lcdNumber.setPalette(palette)
-
-    def tick(self, time):
-        displayTime = QtCore.QTime(0, (time / 60000) % 60, (time / 1000) % 60)
-        self.lcdNumber.display(displayTime.toString('mm:ss'))
-
-    def stateChanged(self, newState, oldState):
-        if newState == Phonon.ErrorState:
-            if self.mediaObject.errorType() == Phonon.FatalError:
-                QtGui.QMessageBox.warning(self, "Fatal Error",
-                                          self.mediaObject.errorString())
-            else:
-                QtGui.QMessageBox.warning(self, "Error",
-                                          self.mediaObject.errorString())
-
-        elif newState == Phonon.PlayingState:
-            self.audioPlayBtn.setEnabled(False)
-            self.audioPauseBtn.setEnabled(True)
-            self.audioStopBtn.setEnabled(True)
-
-        elif newState == Phonon.StoppedState:
-            self.audioStopBtn.setEnabled(False)
-            self.audioPlayBtn.setEnabled(True)
-            self.audioPauseBtn.setEnabled(False)
-            self.lcdNumber.display("00:00")
-
-        elif newState == Phonon.PausedState:
-            self.audioPauseBtn.setEnabled(False)
-            self.audioStopBtn.setEnabled(True)
-            self.audioPlayBtn.setEnabled(True)
 
     def closeEvent(self, event):
         reply = QtGui.QMessageBox.question(self, 'Message',
